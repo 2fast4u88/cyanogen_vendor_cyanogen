88174d4c3cf9c18910ae1b10399264eb0415132d
2fast4u88 <bk95ta391@gmail.com>
2011-10-17 13:37:09 -0300

    Remove themes

--- products/common_full.mk
+++ products/common_full.mk
@@ -4,9 +4,6 @@ $(call inherit-product, vendor/cyanogen/products/common.mk)
 # Bring in all audio files
 include frameworks/base/data/sounds/AllAudio.mk
 
-# Theme packages
-include vendor/cyanogen/products/themes.mk
-
 # Include extra dictionaries for LatinIME
 PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/dictionaries
 
